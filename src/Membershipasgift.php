68d29abce5d98b8c1f41386c61346c87e2fe5184
WAGOOD <duke1999@gmail.com>
2019-10-09 16:37:36 +0300

    try to install

--- src/Membershipasgift.php
+++ src/Membershipasgift.php
@@ -42,126 +42,148 @@ use yii\base\Event;
  *
  * @property  GiftServiceService $giftService
  */
-class Membershipasgift extends Plugin
+class MembershipAsGift extends Plugin
 {
-    // Static Properties
-    // =========================================================================
+  // Static Properties
+  // =========================================================================
+
+  /**
+   * Static property that is an instance of this plugin class so that it can be accessed via
+   * Membershipasgift::$plugin
+   *
+   * @var Membershipasgift
+   */
+  public static $plugin;
+
+  // Public Properties
+  // =========================================================================
+
+  /**
+   * To execute your plugin’s migrations, you’ll need to increase its schema version.
+   *
+   * @var string
+   */
+  public $schemaVersion = '0.0.1';
+
+  // Public Methods
+  // =========================================================================
+
+  /**
+   * Set our $plugin static property to this class so that it can be accessed via
+   * Membershipasgift::$plugin
+   *
+   * Called after the plugin class is instantiated; do any one-time initialization
+   * here such as hooks and events.
+   *
+   * If you have a '/vendor/autoload.php' file, it will be loaded for you automatically;
+   * you do not need to load it in your init() method.
+   *
+   */
+  public function init()
+  {
+    parent::init();
+    self::$plugin = $this;
+
+    $this->_registerCpRoutes();
+
+    // Register our site routes
+    Event::on(
+        UrlManager::class,
+        UrlManager::EVENT_REGISTER_SITE_URL_RULES,
+        function (RegisterUrlRulesEvent $event) {
+          $event->rules['siteActionTrigger1'] = 'membership-as-gift/gift-controller';
+        }
+    );
+
+    // Register our CP routes
+    Event::on(
+        UrlManager::class,
+        UrlManager::EVENT_REGISTER_CP_URL_RULES,
+        function (RegisterUrlRulesEvent $event) {
+          $event->rules['cpActionTrigger1'] = 'membership-as-gift/gift-controller/do-something';
+        }
+    );
+
+    // Register our fields
+    Event::on(
+        Fields::class,
+        Fields::EVENT_REGISTER_FIELD_TYPES,
+        function (RegisterComponentTypesEvent $event) {
+          $event->types[] = GiftFieldField::class;
+        }
+    );
+
+    // Register our variables
+    Event::on(
+        CraftVariable::class,
+        CraftVariable::EVENT_INIT,
+        function (Event $event) {
+          /** @var CraftVariable $variable */
+          $variable = $event->sender;
+          $variable->set('membershipasgift', MembershipasgiftVariable::class);
+        }
+    );
+
+    // Do something after we're installed
+    Event::on(
+        Plugins::class,
+        Plugins::EVENT_AFTER_INSTALL_PLUGIN,
+        function (PluginEvent $event) {
+          if ($event->plugin === $this) {
+            // We were just installed
+          }
+        }
+    );
 
     /**
-     * Static property that is an instance of this plugin class so that it can be accessed via
-     * Membershipasgift::$plugin
+     * Logging in Craft involves using one of the following methods:
      *
-     * @var Membershipasgift
+     * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
+     * Craft::info(): record a message that conveys some useful information.
+     * Craft::warning(): record a warning message that indicates something unexpected has happened.
+     * Craft::error(): record a fatal error that should be investigated as soon as possible.
+     *
+     * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
+     *
+     * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
+     * the category to the method (prefixed with the fully qualified class name) where the constant appears.
+     *
+     * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
+     * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
+     *
+     * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
      */
-    public static $plugin;
+    Craft::info(
+        Craft::t(
+            'membership-as-gift',
+            '{name} plugin loaded',
+            ['name' => $this->name]
+        ),
+        __METHOD__
+    );
+  }
 
-    // Public Properties
-    // =========================================================================
+  // Protected Methods
+  // =========================================================================
+  private function _registerCpRoutes()
+  {
+    Event::on(UrlManager::class, UrlManager::EVENT_REGISTER_CP_URL_RULES, function(RegisterUrlRulesEvent $event) {
+      $event->rules = array_merge($event->rules, [
+          'members/activation/<voucherId>' => 'membership-as-gift/voucher/activate',
+          /**'gift-voucher/voucher-types/<voucherTypeId:\d+>' => 'gift-voucher/voucher-types/edit',
 
-    /**
-     * To execute your plugin’s migrations, you’ll need to increase its schema version.
-     *
-     * @var string
-     */
-    public $schemaVersion = '0.0.1';
+          'gift-voucher/vouchers/<voucherTypeHandle:{handle}>' => 'gift-voucher/vouchers/index',
+          'gift-voucher/vouchers/<voucherTypeHandle:{handle}>/new' => 'gift-voucher/vouchers/edit',
+          'gift-voucher/vouchers/<voucherTypeHandle:{handle}>/new/<siteHandle:\w+>' => 'gift-voucher/vouchers/edit',
+          'gift-voucher/vouchers/<voucherTypeHandle:{handle}>/<voucherId:\d+>' => 'gift-voucher/vouchers/edit',
+          'gift-voucher/vouchers/<voucherTypeHandle:{handle}>/<voucherId:\d+>/<siteHandle:\w+>' => 'gift-voucher/vouchers/edit',
 
-    // Public Methods
-    // =========================================================================
+          'gift-voucher/codes/new' => 'gift-voucher/codes/edit',
+          'gift-voucher/codes/<codeId:\d+>' => 'gift-voucher/codes/edit',
 
-    /**
-     * Set our $plugin static property to this class so that it can be accessed via
-     * Membershipasgift::$plugin
-     *
-     * Called after the plugin class is instantiated; do any one-time initialization
-     * here such as hooks and events.
-     *
-     * If you have a '/vendor/autoload.php' file, it will be loaded for you automatically;
-     * you do not need to load it in your init() method.
-     *
-     */
-    public function init()
-    {
-        parent::init();
-        self::$plugin = $this;
-
-        // Register our site routes
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_SITE_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                $event->rules['siteActionTrigger1'] = 'membership-as-gift/gift-controller';
-            }
-        );
-
-        // Register our CP routes
-        Event::on(
-            UrlManager::class,
-            UrlManager::EVENT_REGISTER_CP_URL_RULES,
-            function (RegisterUrlRulesEvent $event) {
-                $event->rules['cpActionTrigger1'] = 'membership-as-gift/gift-controller/do-something';
-            }
-        );
-
-        // Register our fields
-        Event::on(
-            Fields::class,
-            Fields::EVENT_REGISTER_FIELD_TYPES,
-            function (RegisterComponentTypesEvent $event) {
-                $event->types[] = GiftFieldField::class;
-            }
-        );
-
-        // Register our variables
-        Event::on(
-            CraftVariable::class,
-            CraftVariable::EVENT_INIT,
-            function (Event $event) {
-                /** @var CraftVariable $variable */
-                $variable = $event->sender;
-                $variable->set('membershipasgift', MembershipasgiftVariable::class);
-            }
-        );
-
-        // Do something after we're installed
-        Event::on(
-            Plugins::class,
-            Plugins::EVENT_AFTER_INSTALL_PLUGIN,
-            function (PluginEvent $event) {
-                if ($event->plugin === $this) {
-                    // We were just installed
-                }
-            }
-        );
-
-/**
- * Logging in Craft involves using one of the following methods:
- *
- * Craft::trace(): record a message to trace how a piece of code runs. This is mainly for development use.
- * Craft::info(): record a message that conveys some useful information.
- * Craft::warning(): record a warning message that indicates something unexpected has happened.
- * Craft::error(): record a fatal error that should be investigated as soon as possible.
- *
- * Unless `devMode` is on, only Craft::warning() & Craft::error() will log to `craft/storage/logs/web.log`
- *
- * It's recommended that you pass in the magic constant `__METHOD__` as the second parameter, which sets
- * the category to the method (prefixed with the fully qualified class name) where the constant appears.
- *
- * To enable the Yii debug toolbar, go to your user account in the AdminCP and check the
- * [] Show the debug toolbar on the front end & [] Show the debug toolbar on the Control Panel
- *
- * http://www.yiiframework.com/doc-2.0/guide-runtime-logging.html
- */
-        Craft::info(
-            Craft::t(
-                'membership-as-gift',
-                '{name} plugin loaded',
-                ['name' => $this->name]
-            ),
-            __METHOD__
-        );
-    }
-
-    // Protected Methods
-    // =========================================================================
+          'gift-voucher/settings' => 'gift-voucher/base/settings',**/
+      ]);
+    });
+  }
 
 }
